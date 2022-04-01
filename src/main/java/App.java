f95408573e93b32577b95874185b1f3e3a3b7cef
Your Name <you@example.com>
2022-04-01 14:50:22 +0300

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,25 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld secondBean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean==secondBean);
+        //System.out.println(bean.getMessage());
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat secondCatBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean==secondCatBean);
+
+
+
+
+
+
     }
 }
